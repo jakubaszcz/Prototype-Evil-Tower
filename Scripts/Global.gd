abd359a05d8c9ba4e920580f23778af9c50c37a1
jakub <jakub.szczucinski@epitech.eu>
2025-09-19 08:57:30 +0200

    [PET] Initialize Global File

---
+++ Scripts/Global.gd
@@ -0,0 +1,9 @@
+extends Node
+
+# Progression file
+var progression_file_extension := "cfg"
+var progression_file := "progression" + "." + progression_file_extension
+var progression_path := "user://" + progression_file
+
+# Ressources
+var ressources_coins := 0
