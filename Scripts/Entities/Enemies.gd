373e167d5554bf5a8e44503dea5d8fcf14eaf557
jakub <jakub.szczucinski@epitech.eu>
2025-09-19 15:00:08 +0200

    [PET] Enemies following target

---
+++ Scripts/Entities/Enemies.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+var enemy_speed := 200.0
+@onready var player: CharacterBody2D = $"../../Player/Player"
+
+func _physics_process(delta: float) -> void:
+	if !player: return
+	
+	var direction = (player.global_position - self.global_position).normalized()
+	velocity = direction * enemy_speed
+	move_and_slide()
