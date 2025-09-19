59185d9fad22702fcf9c8766cf81cd704a24c07c
jakub <jakub.szczucinski@epitech.eu>
2025-09-19 09:24:18 +0200

    [PET] Create a saving system

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -7,3 +7,16 @@ var progression_path := "user://" + progression_file
 
 # Ressources
 var ressources_coins := 0
+
+func load_progression():
+	var config := ConfigFile.new()
+	var error = config.load(progression_path)
+	if error == OK:
+		Global.ressources_coins = config.get_value("progress", "coins", 0)
+	else:
+		print("No save hve been found.")
+
+func save_progression():
+	var config = ConfigFile.new()
+	config.set_value("progress", "coins", ressources_coins)
+	config.save(progression_path)
