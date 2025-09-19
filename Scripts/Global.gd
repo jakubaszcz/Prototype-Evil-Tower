899a4a0ab30a14def931a74cdc4a7c7f2a4a492d
jakub <jakub.szczucinski@epitech.eu>
2025-09-19 10:17:32 +0200

    [PET] Adding Abilities For The Player

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -6,19 +6,30 @@ var progression_file := "progression" + "." + progression_file_extension
 var progression_path := "user://" + progression_file
 
 # Ressources
-var ressources_coins := 0
+var ressources_coins := 0 # IG Coins Ressources
+
+# Abilities
+var abilities_attack := 0 # Global Abilities, That Affects IG
+var abilities_radius := 0 # Global Abilities, That Affects IG
+var abilities_health := 0 # Global Abilities, That Affects IG
 
 # Load Progression
 func load_progression():
 	var config := ConfigFile.new()
 	var error = config.load(progression_path)
 	if error == OK:
-		Global.ressources_coins = config.get_value("progress", "coins", 0)
+		ressources_coins = config.get_value("progress", "coins", 0)
+		abilities_health = config.get_value("progress", "attack", 10)
+		abilities_radius = config.get_value("progress", "radius", 10)
+		abilities_health = config.get_value("progress", "health", 10)
 	else:
-		print("No save hve been found.")
+		print("No save have been found.")
 
 # Save Progression
 func save_progression():
 	var config = ConfigFile.new()
-	config.set_value("progress", "coins", ressources_coins)
+	config.get_value("progress", "coins", ressources_coins)
+	config.get_value("progress", "attack", abilities_attack)
+	config.get_value("progress", "radius", abilities_radius)
+	config.get_value("progress", "health", abilities_health)
 	config.save(progression_path)
