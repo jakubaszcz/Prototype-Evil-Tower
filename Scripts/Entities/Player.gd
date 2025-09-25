0288951b04adccbaec9e2f8cecedf7ca729d9f36
jakub <jakub.szczucinski@epitech.eu>
2025-09-25 22:19:16 +0200

    [PET] Shoot Even In Area

--- Scripts/Entities/Player.gd
+++ Scripts/Entities/Player.gd
@@ -4,10 +4,19 @@ extends CharacterBody2D
 @onready var shoot_cooldown = $ShootCooldown 
 
 @onready var projectile_scene = load("res://Components/Utils/Projectiles.tscn")
+@onready var detection_area: Area2D = $RadiusArea
+
+func _physics_process(delta: float) -> void:
+	var overlaps = detection_area.get_overlapping_areas()
+	for area in overlaps:
+		if area.is_in_group("enemy") and can_shoot:
+			_shoot(area)
 
 func _on_radius_area_body_entered(body: Node2D) -> void:
 	if body.is_in_group("enemy") and can_shoot:
 		_shoot(body)
+		
+
 
 func _shoot(body: Node2D) -> void:
 	var projectile = projectile_scene.instantiate()
