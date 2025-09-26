97b946732f57972527b4a92752b264314a81563b
jakub <jakub.szczucinski@epitech.eu>
2025-09-26 10:11:44 +0200

    [PET] Can Shoot With Overlapping

--- Scripts/Entities/Player.gd
+++ Scripts/Entities/Player.gd
@@ -7,21 +7,21 @@ extends CharacterBody2D
 @onready var detection_area: Area2D = $RadiusArea
 
 func _physics_process(delta: float) -> void:
-	var overlaps = detection_area.get_overlapping_areas()
-	for area in overlaps:
-		if area.is_in_group("enemy") and can_shoot:
-			_shoot(area)
+	if not can_shoot: return
+	for overlaps in detection_area.get_overlapping_bodies():
+		if overlaps.is_in_group("enemy"):
+			print("there is someone")
+			_shoot(overlaps)
 
 func _on_radius_area_body_entered(body: Node2D) -> void:
 	if body.is_in_group("enemy") and can_shoot:
 		_shoot(body)
-		
-
 
 func _shoot(body: Node2D) -> void:
 	var projectile = projectile_scene.instantiate()
+	can_shoot = false
 	projectile.global_position = global_position
-	projectile.target_position = body.global_position
+	projectile._set_target(body)
 	get_parent().add_child(projectile)
 	
 
