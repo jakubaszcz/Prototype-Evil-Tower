5b276f125d4fa9984ca23dbf4b2680319dd39422
jakub <jakub.szczucinski@epitech.eu>
2025-10-02 11:44:54 +0200

    [PET] Creating shop

--- Components/Interfaces/Game/PlayerStatistics.gd
+++ Components/Interfaces/Game/PlayerStatistics.gd
@@ -3,21 +3,44 @@ extends Node
 @onready var infos_label = $Infos
 
 func _physics_process(_delta: float) -> void:
-	infos_label.text =  "Coins   : " + str(Game.game_coin)   + "\n" \
-					  + "Health  : " + str(Game.game_health) + "\n" \
-					  + "Radius  : " + str(Game.game_radius) + "\n" \
-					  + "Cadence : " + str(Game.game_cadence) + "\n" \
-					  + "Damage  : " + str(Game.game_damage)
+	$DamageContainer/Price.text = "Price : " + str(Game.game_damage_price)
+	$DamageContainer/Ability.text = "Damage : " + str(Game.game_damage)
+	
+	$HealthContainer/Price.text = "Price : " + str(Game.game_health_price)
+	$HealthContainer/Ability.text = "Health : " + str(Game.game_health)
+	
+	$RadiusContainer/Price.text = "Price : " + str(Game.game_radius_price)
+	$RadiusContainer/Ability.text = "Radius : " + str(Game.game_radius)
+	
+	$CadenceContainer/Price.text = "Price : " + str(Game.game_cadence_price)
+	$CadenceContainer/Ability.text = "Cadence : " + str(Game.game_cadence)
+	
+	infos_label.text =  str(Game.game_coin)
 
 
-func _on_damagae_pressed() -> void:
-	Game.game_damage += 1
+func _on_damage_pressed() -> void:
+	if ((Game.game_coin - Game.game_damage_price) >= 0):
+		Game.game_coin -= Game.game_damage_price
+		Game.game_damage *= Game.game_damage_buy
+		Game.game_damage_price *= Game.multiplier
+
 
 func _on_health_pressed() -> void:
-	Game.game_health += 1
+		if ((Game.game_coin - Game.game_health_price) >= 0):
+			Game.game_coin -= Game.game_health_price
+			Game.game_health *= Game.game_health_buy
+			Game.game_health_price *= Game.multiplier
+
 
 func _on_radius_pressed() -> void:
-	Game.game_radius += 1
+	if ((Game.game_coin - Game.game_radius_price) >= 0):
+		Game.game_coin -= Game.game_radius_price
+		Game.game_radius *= Game.game_radius_buy
+		Game.game_radius_price *= Game.multiplier
+
 
 func _on_cadence_pressed() -> void:
-	Game.game_cadence -= 0.5
+	if ((Game.game_coin - Game.game_cadence_price) >= 0):
+		Game.game_coin -= Game.game_cadence_price
+		Game.game_cadence *= Game.game_cadence_buy
+		Game.game_cadence_price *= Game.multiplier
--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -6,3 +6,17 @@ var game_health : float = 10 + Global.abilities_health
 var game_radius : float = 10.0 + Global.abilities_radius
 var game_cadence : float = 2.0 + Global.abilities_cadence
 var game_damage : float = 10.0 + Global.abilities_attack
+
+# Base Shop Prices
+
+var game_damage_price : int = 7
+var game_health_price : int = 8
+var game_radius_price : int = 9
+var game_cadence_price : int = 10
+
+var game_damage_buy : float = 0.7
+var game_health_buy : float = 2.0
+var game_radius_buy : float = 0.4
+var game_cadence_buy : float = 0.3
+
+var multiplier : float = 1.2
