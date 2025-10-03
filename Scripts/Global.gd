c44a1f4cba6556e3b9655868c890cc9a2756421f
jakub <jakub.szczucinski@epitech.eu>
2025-10-03 14:32:15 +0200

    [PET] Menu Shop

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -4,10 +4,31 @@ extends Node
 var progression_path := "user://progression.cfg"
 
 var ressources_gems : int = 0
-var abilities_attack : float = 10.0
+var abilities_attack : float = 0.0
 var abilities_radius : float = 0.0
 var abilities_health : float = 0.0
 var abilities_cadence : float = 0.0
+
+var abilities_attack_price : int = 12
+var abilities_radius_price : int = 13
+var abilities_health_price : int = 15
+var abilities_cadence_price : int = 17
+
+var abilities_attack_price_multiplier : float = 1.2
+var abilities_radius_price_multiplier : float = 1.4
+var abilities_health_price_multiplier : float = 1.6
+var abilities_cadence_price_multiplier : float = 1.8
+
+var abilities_attack_reward : int = 2
+var abilities_radius_reward : float = 0.2
+var abilities_health_reward : int = 3
+var abilities_cadence_reward : float = 0.3
+
+var abilities_attack_shop_level : int = 1
+var abilities_radius_shop_level : int = 1
+var abilities_health_shop_level : int = 1
+var abilities_cadence_shop_level : int = 1
+
 var start_coin : int = 0
 
 func load_progression():
@@ -20,9 +41,10 @@ func load_progression():
 		abilities_health  = config.get_value("progress", "health", 0.0)
 		abilities_cadence = config.get_value("progress", "cadence", 0.0)
 		start_coin        = config.get_value("progress", "coin", 0)
-		print("Save chargée :", ProjectSettings.globalize_path(progression_path))
-	else:
-		print("Pas de save trouvée, valeurs par défaut.")
+		abilities_attack_shop_level = config.get_value("shop", "attack_shop_level", 1)
+		abilities_radius_shop_level = config.get_value("shop", "radius_shop_level", 1)
+		abilities_health_shop_level = config.get_value("shop", "health_shop_level", 1)
+		abilities_cadence_shop_level = config.get_value("shop", "cadence_shop_level", 1)
 
 func save_progression():
 	var config = ConfigFile.new()
@@ -32,9 +54,8 @@ func save_progression():
 	config.set_value("progress", "health", abilities_health)
 	config.set_value("progress", "cadence", abilities_cadence)
 	config.set_value("progress", "coin", start_coin)
-
-	var err = config.save(progression_path)
-	if err != OK:
-		push_error("Erreur de sauvegarde : " + str(err))
-	else:
-		print("💾 Sauvegarde réussie :", ProjectSettings.globalize_path(progression_path))
+	config.set_value("shop", "attack_shop_level", abilities_attack_shop_level)
+	config.set_value("shop", "radius_shop_level", abilities_radius_shop_level)
+	config.set_value("shop", "health_shop_level", abilities_health_shop_level)
+	config.set_value("shop", "cadence_shop_level", abilities_cadence_shop_level)
+	config.save(progression_path)
--- Scripts/Scenes/Menu.gd
+++ Scripts/Scenes/Menu.gd
@@ -2,7 +2,70 @@ extends Node2D
 
 func _ready() -> void:
 	Global.load_progression()
-	$CanvasLayer/Gems.text = "Gems : " + str(Global.ressources_gems)
+	
+	
 
-func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Components/Scenes/Game.tscn")
+
+
+func _physics_process(delta: float) -> void:
+	
+	$CanvasLayer/Gems.text = "Gems : " + str(Global.ressources_gems)
+	
+	# Damage
+	$CanvasLayer/Shop/Damage/Price.text = "Price : " +str(Global.abilities_attack_price)
+	$CanvasLayer/Shop/Damage/Bonus.text = "Bonus Damage : " + str(Global.abilities_attack)
+	# Health
+	$CanvasLayer/Shop/Health/Price.text = "Price : " +str(Global.abilities_health_price)
+	$CanvasLayer/Shop/Health/Bonus.text = "Bonus Health : " + str(Global.abilities_health)
+	# Radius
+	$CanvasLayer/Shop/Radius/Price.text = "Price : " +str(Global.abilities_radius_price)
+	$CanvasLayer/Shop/Radius/Bonus.text = "Bonus Radius : " + str(Global.abilities_radius)
+	# Cadence
+	$CanvasLayer/Shop/Cadence/Price.text = "Price : " +str(Global.abilities_cadence_price)
+	$CanvasLayer/Shop/Cadence/Bonus.text = "Bonus Cadence : " + str(Global.abilities_cadence)
+
+
+func _on_damage_pressed() -> void:
+	if Global.ressources_gems >= Global.abilities_attack_price:
+		Global.ressources_gems -= Global.abilities_attack_price
+		Global.abilities_attack_price = 12 * pow(Global.abilities_attack_price_multiplier, Global.abilities_attack_shop_level)
+		Global.abilities_attack += Global.abilities_attack_reward
+		Global.abilities_attack_shop_level += 1
+		
+		# Save
+		Global.save_progression()
+
+func _on_health_pressed() -> void:
+	if Global.ressources_gems >= Global.abilities_health_price:
+		Global.ressources_gems -= Global.abilities_health_price
+		Global.abilities_health_price = 13 * pow(Global.abilities_health_price_multiplier, Global.abilities_health_shop_level)
+		Global.abilities_health += Global.abilities_health_reward
+		Global.abilities_health_shop_level += 1
+		
+		# Save
+		Global.save_progression()
+
+
+func _on_radius_pressed() -> void:
+	if Global.ressources_gems >= Global.abilities_radius_price:
+		Global.ressources_gems -= Global.abilities_radius_price
+		Global.abilities_radius_price = 15 * pow(Global.abilities_radius_price_multiplier, Global.abilities_radius_shop_level)
+		Global.abilities_radius += Global.abilities_radius_reward
+		Global.abilities_radius_shop_level += 1
+		
+		# Save
+		Global.save_progression()
+
+func _on_cadence_pressed() -> void:
+	if Global.ressources_gems >= Global.abilities_cadence_price:
+		Global.ressources_gems -= Global.abilities_cadence_price
+		Global.abilities_cadence_price = 17 * pow(Global.abilities_cadence_price_multiplier, Global.abilities_cadence_shop_level)
+		Global.abilities_cadence += Global.abilities_cadence_reward
+		Global.abilities_cadence_shop_level += 1
+		
+		# Save
+		Global.save_progression()
+
+
+func _on_play_pressed() -> void:
+		get_tree().change_scene_to_file("res://Components/Scenes/Game.tscn")
